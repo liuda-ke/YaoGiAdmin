4ecf0c2645efb71342388a52c0fb1b1401e18d95
YaoGi <YaoGi@DESKTOP-KDIUB4U>
2021-02-21 21:32:22 +0800

    add extension files of json and datetime

---
+++ YaoGiAdmin.Core/Extensions/DateTimeExtension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaoGiAdmin.Core.Extensions
+{
+    public static class DateTimeExtension
+    {
+        /// <summary>
+        /// 将日期字符串转为时间
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="timestr"></param>
+        /// <returns></returns>
+        public static DateTime FromDateTime<T>(this string timeStr)
+        {
+            if (!string.IsNullOrWhiteSpace(timeStr))
+            {
+                DateTime dt = new DateTime();
+                bool flag = DateTime.TryParse(timeStr, out dt);
+                if (flag)
+                {
+                    return dt;
+                }
+                else
+                {
+                    return default;
+                }
+            }
+            else
+            {
+                return default;
+            }
+        }
+        /// <summary>
+        /// 将日期转为yyyy-MM-dd HH:mm:ss时间格式
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="timestr"></param>
+        /// <returns></returns>
+        public static string FromDateTime<T>(this DateTime timeStr)
+        {
+            if (timeStr != DateTime.MinValue)
+            {
+                return timeStr.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+
+
+    }
+}
---
+++ YaoGiAdmin.Core/Extensions/JsonExtension.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaoGiAdmin.Core.Extensions
+{
+    public static class JsonExtension
+    {
+        /// <summary>
+        /// 泛型转Json字符串
+        /// </summary>
+        /// <typeparam name="T">泛型</typeparam>
+        /// <param name="t">泛型</param>
+        /// <returns></returns>
+        public static string ToJson<T>(this T t)
+        {
+            if (t != null)
+            {
+                return JsonConvert.SerializeObject(t);
+            }
+            else
+            {
+                return "";
+            }
+        }
+        /// <summary>
+        /// Json字符串转实体
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="jsonStr">json字符串</param>
+        /// <returns></returns>
+        public static T FromJson<T>(this string jsonStr)
+        {
+            if (!string.IsNullOrWhiteSpace(jsonStr))
+            {
+                return JsonConvert.DeserializeObject<T>(jsonStr);
+            }
+            else
+            {
+                return default;
+            }
+        }
+    }
+}
