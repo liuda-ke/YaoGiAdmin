e0e60a7ab37cdae975135facb601708ce78fd2a8
YaoGi <1366505724@qq.com>
2021-05-30 13:20:10 +0800

    添加字段扩展类

---
+++ YaoGiAdmin.CodeGenerate/Extension/ColumnExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaoGiAdmin.CodeGenerate.Extension
+{
+    /// <summary>
+    /// 字段扩展类
+    /// </summary>
+    public class ColumnExtension
+    {
+        /// <summary>
+        /// 字段名称
+        /// </summary>
+        public string ColumnName { get; set; }
+        /// <summary>
+        /// 字段类型
+        /// </summary>
+        public string ColumnType { get; set; }
+        /// <summary>
+        /// 是否是主键
+        /// </summary>
+        public bool IsPrimaryKey { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Comment { get; set; }
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string TableName { get; set; }
+
+    }
+}
